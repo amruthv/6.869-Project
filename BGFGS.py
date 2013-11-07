d3864932cfba9ee754c10afade6addf31cb98799
Amruth Venkatraman <amruthvenkatraman@Amruths-MacBook-Air.local>
2013-11-07 17:22:38 -0500

    sigmoid accumulator

--- BGFGS.py
+++ BGFGS.py
@@ -2,36 +2,47 @@ import numpy as np
 import cv2
 import cv2.cv as cv
 import utils
+import math
 
-vidFile = cv2.VideoCapture( './testvid.mp4' )
 
-nFrames = int(vidFile.get(cv.CV_CAP_PROP_FRAME_COUNT) )
-fps = vidFile.get(cv.CV_CAP_PROP_FPS )
-waitPerFrameInMillisec = int( 1/fps * 1000/1 )
+def sigmoid(z):
+	return 1/(1+math.exp(-10*(z-.5)));
 
-print 'Num. Frames = ', nFrames
-print 'Frame Rate = ', fps, ' frames per sec'
-s,background=vidFile.read()
-# background = utils.rotateImg(background)
-# background *= 1./255 
-print background.shape
-background=cv2.cvtColor(background,cv2.COLOR_RGB2GRAY)
-background = background/255.0
-print background.shape
-print np.amax(background)
+def weightVideo(vidName):
+	vidFile = cv2.VideoCapture(vidName)
 
-mu = 0.01
+	nFrames = int(vidFile.get(cv.CV_CAP_PROP_FRAME_COUNT) )
+	fps = vidFile.get(cv.CV_CAP_PROP_FPS )
+	waitPerFrameInMillisec = int( 1/fps * 1000/1 )
 
-for f in xrange( nFrames ):
-  s,frameImg = vidFile.read()
-  gray = cv2.cvtColor(frameImg,cv2.COLOR_RGB2GRAY)
-  gray = gray / 255.0
-  background = (1-mu) * background + mu * gray
-  # print background
-  # background=cv2.cvtColor(background,cv2.CV_8U)
-  cv2.imshow( "Original Frame",  utils.rotateImg(frameImg))	
-  cv2.imshow( "My Video Window",  utils.rotateImg(background))
-  cv2.waitKey( waitPerFrameInMillisec  )
+	print 'Num. Frames = ', nFrames
+	print 'Frame Rate = ', fps, ' frames per sec'
+	s,background=vidFile.read()
+	shape = background.shape
+	background=cv2.cvtColor(background,cv2.COLOR_RGB2GRAY)
+	background = background/255.0
+
+
+	mu = 0.1
+
+	for f in xrange( nFrames -1 ):
+	  s,frameImg = vidFile.read()
+	  gray = cv2.cvtColor(frameImg,cv2.COLOR_RGB2GRAY)
+	  gray = gray / 255.0
+	  background = (1-mu) * background + mu * gray
+	  for i in range(shape[0]):
+	  	print i
+	  	for j in range(shape[1]):
+	  		z = background[i,j]
+	  		background[i,j] = sigmoid(z*1.0)
+
+	  # print background
+	  # background=cv2.cvtColor(background,cv2.CV_8U)
+	  cv2.imshow( "Original Frame",  utils.rotateImg(frameImg))	
+	  cv2.imshow( "My Video Window",  utils.rotateImg(background))
+	  cv2.waitKey( waitPerFrameInMillisec  )
+
+weightVideo('testvid.mp4')
 
 
 # # When playing is done, delete the window
