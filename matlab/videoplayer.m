5324e4ee120c225c3381eab57fa7379597e4837f
Yajit <yajit@dhcp-18-111-24-7.dyn.mit.edu>
2013-11-13 12:22:35 -0500

    added two smaller videos

--- matlab/videoplayer.m
+++ matlab/videoplayer.m
@@ -1,16 +1,17 @@
-videoFReader = vision.VideoFileReader('markertest.mp4')
+videoFReader = vision.VideoFileReader('4lettertest.mp4')
 EOF=0;
 framecounter=0
 [initialframe, EOF] = step(videoFReader);
 background=ones(size(rgb2gray(initialframe)));
-mu=0.1
+mu=0.05
 
-videoFWriter = vision.VideoFileWriter('ouput.mp4','FrameRate',videoFReader.info.VideoFrameRate);
+videoFWriter = vision.VideoFileWriter('ouput4.avi','FrameRate',videoFReader.info.VideoFrameRate);
 
 while EOF==0 
     [frame, EOF] = step(videoFReader);
     framecounter=framecounter+1
     grayframe=rgb2gray(frame);
+    %grayframe=mat2gray(retinexfunc(255.0*grayframe));
     background = (1-mu) * background + mu * grayframe;
     background=mat2gray(retinexfunc(255.0*background));
     imshow(background);
