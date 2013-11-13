a680de77d0f5dbbfad7862f40f747e3c2d1c01da
Yajit <yajit@dhcp-18-111-24-7.dyn.mit.edu>
2013-11-12 23:24:26 -0500

    writing video

--- matlab/videoplayer.m
+++ matlab/videoplayer.m
@@ -5,7 +5,7 @@ framecounter=0
 background=ones(size(rgb2gray(initialframe)));
 mu=0.1
 
-videoFWriter = vision.VideoFileWriter('ouput','FrameRate',videoFReader.info.VideoFrameRate);
+videoFWriter = vision.VideoFileWriter('ouput.avi','FrameRate',videoFReader.info.VideoFrameRate);
 
 while EOF==0 
     [frame, EOF] = step(videoFReader);
@@ -15,4 +15,12 @@ while EOF==0
     background=mat2gray(retinexfunc(255.0*background));
     imshow(background);
     pause(0.01);
+    step(videoFWriter, background);
+    if framecounter==30
+        EOF=1
+    end
+    
 end
+
+release(videoFReader);
+release(videoFWriter);
