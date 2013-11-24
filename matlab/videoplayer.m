28b9759528134a3ba5010df893ea4b5f5984abf1
Amruth Venkatraman <amruthvenkatraman@dhcp-18-111-4-245.dyn.mit.edu>
2013-11-24 00:20:14 -0500

    can sample frames in video if too long

--- matlab/videoplayer.m
+++ matlab/videoplayer.m
@@ -1,54 +1,60 @@
-videoFReader = vision.VideoFileReader('4lettertest.mp4')
+function [] = videoplayer(inputFile, frameFreq)
+if ~exist('frameFreq', 'var')
+    frameFreq = 1;
+end
+
+videoFReader = vision.VideoFileReader(inputFile)
 EOF=0;
 framecounter=0
 [initialframe, EOF] = step(videoFReader);
 
 figure;
 imshow(initialframe);
-[x,y]=ginput(4)
+[x,y]=ginput(4);
+close;
 border_mask=poly2mask(x,y,size(initialframe,1),size(initialframe,2));
 
 background=ones(size(rgb2gray(initialframe)));
-output=ones(size(rgb2gray(initialframe)));
+
 mu=0.05
 mu2=0.5
 
-videoFWriter = vision.VideoFileWriter('reconstruction.avi','FrameRate',videoFReader.info.VideoFrameRate);
+videoFWriter = vision.VideoFileWriter('pi.avi','FrameRate',videoFReader.info.VideoFrameRate);
 figure;
-while EOF==0 
-    [frame, EOF] = step(videoFReader);
+while EOF==0
+    for i=1:frameFreq
+        [frame, EOF] = step(videoFReader);
+        if EOF == 1
+            break
+        end
+    end
+    try
+        %compute convex hull of hand
+        [out bin] = generate_skinmap(255.*frame);
+        [Xout,rp]=getLargestCc(logical(bin.*border_mask));
+        CH=bwconvhull(Xout,'objects').*border_mask;
+        mask=1.-CH;
 
-    %compute convex hull of hand
-    [out bin] = generate_skinmap(255.*frame);
-    [Xout,rp]=getLargestCc(logical(bin.*border_mask));
-    CH=bwconvhull(Xout,'objects').*border_mask;
-    mask=1.-CH;
-    
-    %accumulate
-    framecounter=framecounter+1
-    grayframe=rgb2gray(frame);
-    grayframe=mat2gray(retinexfunc(255.0*grayframe));
-    background = (1-mu) * mask.*background + mu * mask.*grayframe+CH.*background;
-    background=mat2gray(retinexfunc(255.0*background));
-    
-     temp=background;
-     temp(temp>.35)=1;
-     temp(temp<=.35)=0;
-     temp=temp.*border_mask;
-%     output = (1-mu2) * output + mu2 * temp;
-%     output(output>.5)=1;
-%     output(output<=.5)=0;
-    rgb_temp(:,:,1)=(temp+(1.-border_mask));
-    rgb_temp(:,:,2)=rgb_temp(:,:,1);
-    rgb_temp(:,:,3)=rgb_temp(:,:,1);
-    reconstruction=initialframe.*rgb_temp;
-     imshow(reconstruction);
-%     pause(0.01);
-    %imshow(mask.*grayframe);
-    
+        %accumulate
+        framecounter=framecounter+1
+        grayframe=rgb2gray(frame);
+        grayframe=mat2gray(retinexfunc(255.0*grayframe));
+        background = (1-mu) * mask.*background + mu * mask.*grayframe+CH.*background;
+        background = mat2gray(retinexfunc(255.0*background));
+    %     
+    %     rgb_temp(:,:,1)=(output+(1.-border_mask));
+    %     rgb_temp(:,:,2)=rgb_temp(:,:,1);
+    %     rgb_temp(:,:,3)=rgb_temp(:,:,1);
+    %     reconstruction=initialframe.*rgb_temp;
+         imshow(background);
+    %     pause(0.01);
+        %imshow(mask.*grayframe);
 
-    pause(0.01);
-    step(videoFWriter,reconstruction);
+
+        pause(0.01);
+        step(videoFWriter,background);
+    catch
+    end
     
 end
 
